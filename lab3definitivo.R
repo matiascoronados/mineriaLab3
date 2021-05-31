9718c9669476c304717fe5329cd399a2a6957eba
Matias <matias.coronado@usach.cl>
2021-05-31 02:45:40 -0400

    Codigo al 80%

--- lab3definitivo.R
+++ lab3definitivo.R
@@ -1,15 +1,14 @@
 library(maxent)
-install.packages("tm")
+#install.packages("tm")
 library(tm)
-library(dismo)
+
 data(iris)
 attach(iris)
 x <- subset(iris,select = -Species)
 y <- Species
 f <- tune.maxent(x,y,nfold=3,showall=TRUE,verbose=TRUE)
-
-install.packages("pkgbuild")
-pkgbuild::check_build_tools()
+#install.packages("pkgbuild")
+#pkgbuild::check_build_tools()
 
 
 # Eficiencia accudracy (los mas altos)
@@ -17,15 +16,10 @@ pkgbuild::check_build_tools()
 # use_gsd: para grandes cantidades de datos
 # set_hellout: cuantos se quedan afuera del entrenamiento
 
-
 model <- maxent(x,y,l1_regularizer = 0.0, l2_regularizer = 0.0, use_sgd = TRUE, set_heldout = 0, verbose = FALSE)
-
 result <- predict(model,x[100:150,])
-
 print(result)
 
-
-
 #Aplicando sobre texto
 #Aplicando sobre texto
 #Aplicando sobre texto
@@ -34,48 +28,50 @@ print(result)
 #Aplicando sobre texto
 #Aplicando sobre texto
 
+data <- read.csv("/Users/matiascoronado/Downloads/datasetcagon.csv", fill=TRUE, header=TRUE, sep=",", row.names=NULL)
 
-dataq <- read.csv("/Users/matiascoronado/Downloads/NYTimes.csv.gz")
-
-corpus <- Corpus(VectorSource(data$Title[1:150]))
+#Conjunto de textos relacionados a cada categoria.
+corpus <- Corpus(VectorSource(data$Text))
 print(corpus)
-summary(corpus)
-inspect(corpus)
-corpus[[1]]$content
-
-#### Pre procesamiento -- ver contexto del problema
+#summary(corpus)
+#inspect(corpus)
+#corpus[[1]]$content
 
 
+###Pre-procesamiento
+
+#Pasar todos los elementos a minusculas
+corpus <- tm_map(corpus, content_transformer(tolower))
 #Remover puntuacion (, ; .)
 corpus <- tm_map(corpus, content_transformer(removePunctuation))
-
-#Elimianar stopwords
+#Remover numeros.
+corpus <- tm_map(corpus, content_transformer(removeNumbers))
+#Eliminar salto de linea.
+toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
+corpus <- tm_map(corpus, toSpace, '\n')
+#Elimianar stopwords.
 corpus <- tm_map(corpus, content_transformer(removeWords),stopwords("english"))
-
-#content_transformer(tolower)
-#content_transformer(removeNumbers)
-#eliminar espacios en blanco
-
+#Eliminar espacios en blanco.
 corpus <- tm_map(corpus, stripWhitespace)
 
-
 #### Aplicando el metodo
-
 # Ver filtros persoalizados para analizar bigramas (ejm. cuenta corriente)
-
 matrix <- DocumentTermMatrix(corpus)
 sparse <- as.compressed.matrix(matrix) 
 
-f <- tune.maxent(sparse[1:100,],data$Topic.Code[1:100],nfold = 3, showall = TRUE, verbose = TRUE)
+# Relacion de datos 1/3.
+f <- tune.maxent(sparse[1:3500,],data$Rating[1:3500],nfold = 3, showall = TRUE, verbose = TRUE)
 print(f)
 
-model <- maxent(sparse[1:100,],data$Topic.Code[1:100],l1_regularizer = 0.2, l2_regularizer = 0.0, use_sgd = FALSE, set_heldout = 0, verbose = FALSE)
+model <- maxent(sparse[1:3500,],data$Rating[1:3500],l1_regularizer = 0.0, l2_regularizer = 1.0, use_sgd = FALSE, set_heldout = 0, verbose = FALSE)
 
-
-results <- predict(model,sparse[101:120,])
+results <- predict(model,sparse[3501:5006,])
 
 #Probabilidad de que alguno de estos casos pertenezcan a una de las categorias (columnas?)
 
 #Hay que identificar cuales osn las etiquetas (label) relevantes
+
+
+
 
 
